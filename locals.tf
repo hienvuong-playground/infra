2338f9885c87b69996aa1ceb49a7a6eef34c2591
Hien Vuong <hienvuong0403@gmail.com>
2026-09-06 00:50:08 +0700

    chore

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
   project_name = "playground"
-  region       = "japaneast"
+  region       = "westeurope"
 }
--- static-web-app.tf
+++ static-web-app.tf
@@ -1,5 +1,5 @@
-# resource "azurerm_static_web_app" "main" {
-#   name                = "stapp-${local.project_name}"
-#   resource_group_name = azurerm_resource_group.main.name
-#   location            = "East Asia"
-# }
+resource "azurerm_static_web_app" "main" {
+  name                = "stapp-${local.project_name}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = local.region
+}
