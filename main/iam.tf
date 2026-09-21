2ffab1104956bffd33a8473d61fa9c53235dd956
Hien Vuong <hienvuong0403@gmail.com>
2026-09-21 23:05:15 +0700

    add cluster admin for hien external

---
+++ main/iam.tf
@@ -0,0 +1,5 @@
+resource "azurerm_role_assignment" "hien_external_aks_rbac_cluster_admin" {
+  principal_id         = "1089f4cb-a8ca-41ab-9bda-684ca1dea96f"
+  role_definition_name = "Azure Kubernetes Service RBAC Cluster Admin"
+  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+}
